bbc33e54291a2df2e066dbd1839b041b56eb5bd4
Theoreoluwa <ireeyy35@gmail.com>
2025-03-26 22:28:06 +0000

    Backend service folder setup

---
+++ server/Cloudinary/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "durxdizwh",
+  api_key: "881819958255964",
+  api_secret: "",
+});
+
+module.exports = cloudinary;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/connection");
+const cors = require("cors");
+const port = 5002;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("start server");
+});
+
+//start server
+app.listen(port, () => {
+  console.log(`start server at port number ${port}`);
+});
